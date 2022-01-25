e742425a57c26505a27bfb57bdf1cb1f5836e45e
rwashi690 <rwashington325@gmail.com>
2022-01-24 22:05:40 -0700

    Completed readme generator by requiring all inputs in the generateMarkdown.js

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# ReadMe_Generator
+# GitHub README
+![GitHub license](https://img.shields.io/badge/license--blue.svg)
+
+## Description
+
+Generates a README document using node.js by entering necessary information in the terminal.
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm -i , npm install inquirer and node index.js
+```
+
+## Usage
+
+undefined
+
+## License
+
+This project is licensed under the  license.
+  
+## Contributing
+
+R. Washington
+
+## Tests
+
+To run tests, run the following command:
+
+```
+none
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at rwashington325@gmail.com. You can find more of my work at [rwashi690](https://github.com/rwashi690/).
+
--- index.js
+++ index.js
@@ -1,38 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMd = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-//const questions = [];
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       message: 'What is your user name?',
-//       name: 'username',
-//     },
-//     {
-//       type: 'password',
-//       message: 'What is your password?',
-//       name: 'password',
-//     },
-//     {
-//       type: 'password',
-//       message: 'Re-enter password to confirm:',
-//       name: 'confirm',
-//     },
-//   ])
-//   .then((response) =>
-//     response.confirm === response.password
-//       ? console.log('Success!')
-//       : console.log('You forgot your password already?!')
-//   );
+const questions = [{
+    type: 'input',
+    message: 'Does the GitHub repository have a license? Leave blank if there is no license',
+    name: 'license'
+},
+{
+    type: 'input',
+    message: 'What is the title of the GitHub repository or project?',
+    name: 'title'
+},
+{
+    type: 'input',
+    message: 'Complete the description of the GitHub repository or project?',
+    name: 'description'
+},
+{
+    type: 'input',
+    message: 'What commands are required to install the project?',
+    name: 'installation'
+},
+{
+    type: 'input',
+    message: 'Who are the project contributors?',
+    name: 'contributing'
+},
+{
+    type: 'input',
+    message: 'What commands are required to run tests with the project?',
+    name: 'test'
+},
+{
+    type: 'input',
+    message: 'What contact email do you want to provide for the ReadMe to address potential issues?',
+    name: 'email'
+},
+{
+    type: 'input',
+    message: 'What are the GitHub accounts of the contributors?',
+    name: 'github'
+}
+]
+
+//inquirer.prompt(questions)
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>
+        err ? console.log(err) : console.log('Read me generated!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            writeToFile('ReadMe.md',generateMd(data))
+        });
+}
 
 // Function call to initialize app
 init();
